93d3bee9dbefcc04ad20ba422292adf3741b9a1f
Steve Yoon <steve@Steves-MacBook-Pro.local>
2022-12-14 19:26:21 -0800

    added function to clean the data

--- predictor.py
+++ predictor.py
@@ -1,11 +1,25 @@
+import pandas as pd
+import numpy as np
+
+class Model():
+    def clean(df):
+        '''
+        (dataframe) -> dataframe
+        Takes the dataframe form of a CSV file and returns a dataframe with specified columns removed
+        '''
+        
+
+        # drops unnecessary columns
+        drop_cols = ['Date','Team','Against','Home','ORB','DRB','+/-','GameLink']
+        df.drop(drop_cols, inplace=True, axis=1)
+
+        # group by players and their stat averages
+        final = df.groupby('Player').mean()
+
+        return final
 
 
-
-
-
-
-
-
-
-if __name__ == '__main__':
-    print('hello world')
+    if __name__ == '__main__':
+        df = pd.read_csv('datasets/2019-20.csv')
+        df.cleaned = clean(df)
+        print(df.cleaned.head())
