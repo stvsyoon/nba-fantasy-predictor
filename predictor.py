ce5f452bd657281f60aae724e270337f4ead9bfc
Steve Yoon <steve@Steves-MacBook-Pro.local>
2022-12-21 00:34:19 -0800

    added season average function

--- predictor.py
+++ predictor.py
@@ -1,29 +1,39 @@
 import pandas as pd
 import numpy as np
 
-class Model():
-    def clean(df):
+class Data:
+    def __init__(self, df):
+        self.df = df
+
+    def clean(self):
         '''
         (dataframe) -> dataframe
         Takes the dataframe form of a CSV file and returns a dataframe with specified columns removed
         '''
         
         # drops unnecessary columns
-        drop_cols = ['Team','Against','Home','ORB','DRB','+/-','GameLink']
-        return df.drop(drop_cols, inplace=False, axis=1)
+        drop_cols = ['Date','Team','Against','Home','ORB','DRB','+/-','GameLink']
+        return self.df.drop(drop_cols, inplace=True, axis=1)
 
-    def get_player_stats(df, name):
+    def get_player_stats(self, name):
         # gets rows with player name and drops all the other rows
-        return df.where(df['Player'] == name).dropna(how='all')
+        return self.df.where(self.df['Player'] == name).dropna(how='all')
 
-    def get_player_names(df):
+    def get_player_names(self):
         # gets all the unique names of players
-        df.drop_duplicates(subset=['Player'], inplace=True)
+        self.df.drop_duplicates(subset=['Player'], inplace=True)
         return np.array(df['Player'])
+    
+    def get_season_average(self, name):
+        df = self.get_player_stats(name)
+        return df.mean()
 
-    if __name__ == '__main__':
-        df = pd.read_csv('datasets/2018-19.csv')
-        df.cleaned = clean(df)
-        test = get_player_stats(df.cleaned, 'Bradley Beal')
 
-        print(get_player_names(df.cleaned))
+
+df = pd.read_csv('datasets/2018-19.csv')
+m = Data(df)
+m.clean()
+test = m.get_player_stats('Bradley Beal')
+# print(m.get_season_average('Bradley Beal'))
+
+
