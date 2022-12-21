1d93a1d5e18dd2c95fa511a7d47ff4b556526cf1
Steve Yoon <steve@Steves-MacBook-Pro.local>
2022-12-20 20:53:01 -0800

    completed functions to get player stats and all unique names. Realized dataset is inaccurate so must get own dataset

--- predictor.py
+++ predictor.py
@@ -8,18 +8,22 @@ class Model():
         Takes the dataframe form of a CSV file and returns a dataframe with specified columns removed
         '''
         
-
         # drops unnecessary columns
-        drop_cols = ['Date','Team','Against','Home','ORB','DRB','+/-','GameLink']
-        df.drop(drop_cols, inplace=True, axis=1)
+        drop_cols = ['Team','Against','Home','ORB','DRB','+/-','GameLink']
+        return df.drop(drop_cols, inplace=False, axis=1)
 
-        # group by players and their stat averages
-        final = df.groupby('Player').mean()
+    def get_player_stats(df, name):
+        # gets rows with player name and drops all the other rows
+        return df.where(df['Player'] == name).dropna(how='all')
 
-        return final
-
+    def get_player_names(df):
+        # gets all the unique names of players
+        df.drop_duplicates(subset=['Player'], inplace=True)
+        return np.array(df['Player'])
 
     if __name__ == '__main__':
-        df = pd.read_csv('datasets/2019-20.csv')
+        df = pd.read_csv('datasets/2018-19.csv')
         df.cleaned = clean(df)
-        print(df.cleaned.head())
+        test = get_player_stats(df.cleaned, 'Bradley Beal')
+
+        print(get_player_names(df.cleaned))
